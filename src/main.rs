14e01975a47df858e42c2bbc5d36dfaaef9faf15
Batur123 <batur.ozkoc@gmail.com>
2021-12-24 14:41:40 +0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#[macro_use] extern crate rocket;
+
+use rocket::{Build, Rocket};
+use rocket::fs::{FileServer, relative};
+
+#[get("/")]
+fn index() -> &'static str
+{
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> // veya -> _
+{
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/public", FileServer::from(relative!("public/")))
+}
